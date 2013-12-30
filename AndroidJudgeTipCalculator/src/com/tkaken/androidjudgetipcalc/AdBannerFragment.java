00607b4a9720311102a501501108a9cf4e7f20a2
tkaken <tka.klein@gmail.com>
2013-12-29 22:33:15 -0500

    [#44] Organize imports.

--- AndroidJudgeTipCalculator/src/com/tkaken/androidjudgetipcalc/AdBannerFragment.java
+++ AndroidJudgeTipCalculator/src/com/tkaken/androidjudgetipcalc/AdBannerFragment.java
@@ -1,6 +1,5 @@
 package com.tkaken.androidjudgetipcalc;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
